354c9126e752252a2cc7785ca991e16eee039e4f
ralphcomplido <rcomplido.la.usa@gmail.com>
2017-09-12 01:06:20 -0700

    trial logic for time running after start game click

---
+++ assets/javascript/app.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+function gameOn() {
+var timer = 10;
+var intervalId;
+
+setTimeout(fiveSeconds, 1000);
+
+ function fiveSeconds() {
+ 	run();
+ 	function run() {
+      intervalId = setInterval(decrement, 1000);
+    }
+
+function decrement() {
+ 	timer--;
+
+ 		$("#question").html("<h2>" + timer + "</h2>");
+ 		if (timer === 0) {
+      	$("#timer").empty();
+      	$("#timer").html("<h2>Time's Up!</h2>");
+      	clearInterval(intervalId);
+      	timer = 10;
+      	gameOn();
+      	
+      }
+        // in the element with an id of time-left add an h2 saying About 10 Seconds Left!
+       }
+        $("#timer").html("<h2>About 10 Seconds Left!</h2>");
+        // console log 10 seconds left
+
+      }
+}
+$("#start-game").on("click", function() {
+   gameOn();
+
+   });
+
+});
