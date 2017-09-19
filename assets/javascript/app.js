adefd6d3dac4a7fb59034ecdfbaf79b31b7cfd56
ralphcomplido <rcomplido.la.usa@gmail.com>
2017-09-19 02:30:50 -0700

    finished homework and added js comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
+    // music
+    var backgroundMusic = new Audio("assets/audio/background-music.mp3");
+    var correctSound = new Audio("assets/audio/correct.mp3");
+    var wrongSound = new Audio("assets/audio/wrong.mp3");
+    var clockSound = new Audio("assets/audio/clock.mp3");
+    var timesupSound = new Audio("assets/audio/times-up.mp3");
+    var clapSound = new Audio("assets/audio/clap.mp3");
+    var cheerSound = new Audio("assets/audio/cheer.mp3");
 
+    // variables for game
     var timer = 10;
     var intervalId;
     var userGuess = 0;
@@ -8,9 +17,9 @@ $(document).ready(function() {
     var noAnswer = 0;
     var correctAnswers = 0;
     var wrongAnswers = 0;
-
+    // start game
     function gameOn() {
-        console.log(askedQuestions.length);
+        // continue game til it reach 10 questions
         if (askedQuestions.length < 10) {
             run();
             $("#start-game").hide();
@@ -18,38 +27,46 @@ $(document).ready(function() {
             $("#times-up").hide();
             $("#timer").html("<h2> Time Remaining: " + timer + "</h2>");
             chooseQuestion();
-        } else {
+        } else { // game result and restart game
+            cheerSound.play();
+            clapSound.play();
+            $("#restart-game").show();
             $("#game-content").hide();
+            $("#times-up").hide();
+            $("#timer").hide();
             $("#game-result").html("<h2><p>Game Over</p></h2>");
             $("#game-result").append("<h2><p>Correct Answers: " + correctAnswers + "</p></h2>");
             $("#game-result").append("<h2><p>Wrong Answers: " + wrongAnswers + "</p></h2>");
             $("#game-result").append("<h2><p>Missed Questions: " + noAnswer + "</p></h2>");
         }
     }
-
+    // set interval for timer
     function run() {
         intervalId = setInterval(decrement, 1000);
 
     };
-
+    // time running
     function decrement() {
+        clockSound.play();
         timer--;
         $("#timer").html("<h2> Time Remaining: " + timer + "</h2>");
-
+        // times up
         if (timer === 0) {
+            timesupSound.play();
+            clockSound.pause();
             noAnswer++;
             timer = 10;
             userGuess = 1;
             $("#options").hide();
             $("#times-up").show();
-            $("#times-up").html("<h2><p>Time's Up!</p><p>The correct answer is: " + questions[currentQuestion].correctAnswer + "</p></h2>");
+            $("#times-up").html("<div style='background:#1e90ff; border-radius: 25px; padding:20px;'><h2><p>Time's Up!</p><p>The correct answer is:</p><p>" + questions[currentQuestion].correctAnswer + "</p></h2></div>");
             clearInterval(intervalId);
             delete questions[currentQuestion];
             setTimeout(gameOn, 3000);
 
         }
     };
-
+    // choose random question
     function chooseQuestion() {
 
         var randomQuestion = function(obj) {
@@ -57,10 +74,7 @@ $(document).ready(function() {
             currentQuestion = keys[keys.length * Math.random() << 0];
 
             askedQuestions.push(currentQuestion);
-            // console.log(keys);
-            // console.log(keys[keys.length * Math.random() << 0])
-            // delete questions[currentQuestion];
-            // console.log(questions[currentQuestion]);
+
             for (var i = 0; i < askedQuestions.length; i++) {
                 $("#options").show();
                 $("#question").html(questions[currentQuestion].question);
@@ -68,42 +82,44 @@ $(document).ready(function() {
                 $("#option-2").html(questions[currentQuestion].b);
                 $("#option-3").html(questions[currentQuestion].c);
                 $("#option-4").html(questions[currentQuestion].d);
-                // console.log(currentQuestion);
-                // console.log(askedQuestions);
-                //  console.log("match") }    
 
             }
         };
         randomQuestion(questions);
-        console.log(questions);
 
     };
+    // choose answer
 
     $(".choose").click(function() {
 
         var answer = this.innerHTML;
+        // if answer is correct
         if (answer === questions[currentQuestion].correctAnswer) {
+            correctSound.play();
+            clockSound.pause();
             correctAnswers++;
             timer = 10;
             console.log("good");
             clearInterval(intervalId);
             $("#options").hide();
             $("#times-up").show();
-            $("#times-up").html("<h2>Your answer is correct! " + questions[currentQuestion].correctAnswer + "</h2>");
+            $("#times-up").html("<div style='background:#00FF00; border-radius: 25px; padding: 35px;'><h2><p>Your answer is correct!</p><p> " + questions[currentQuestion].correctAnswer + "</p></h2></div>");
             clearInterval(intervalId);
             setTimeout(gameOn, 3000);
             userGuess = 0;
             delete questions[currentQuestion];
 
 
-        } else {
+        } else { // if answer is wrong
+            wrongSound.play();
+            clockSound.pause();
             wrongAnswers++;
             timer = 10;
             clearInterval(intervalId);
             console.log("not good");
             $("#options").hide();
             $("#times-up").show();
-            $("#times-up").html("<h2>Your answer is wrong! The Correct answer is : " + questions[currentQuestion].correctAnswer + "</h2>");
+            $("#times-up").html("<div style='background:#ff4500; border-radius: 25px; padding: 20px;'><h2><p>Your answer is wrong!</p><p>The Correct answer is :</p><p>" + questions[currentQuestion].correctAnswer + "<p/></h2></div>");
             clearInterval(intervalId);
             setTimeout(gameOn, 3000);
             userGuess = 0;
@@ -113,9 +129,21 @@ $(document).ready(function() {
     });
 
     $("#options").hide();
+    $("#times-up").hide();
+    $("#restart-game").hide();
+    backgroundMusic.play();
+
+    // start game button
     $("#start-game").on("click", function() {
         $("#game").hide();
+
         gameOn();
+
+
+    });
+    // restart game button
+    $("#restart-game").on("click", function() {
+        window.location.reload();
 
 
     });
